115c560a5f4d96bfef18c85b8e7a51bdd4af330b
Alyssa Wei <wei.alyssa@gmail.com>
2019-05-06 12:03:33 -0700

    Renders tic tac toe table, cells respond to clicks, toggles placing X vs O

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,19 @@
+console.log(`dis gon be gud`);
 
+// Variable that tracks whether current turn is for player1
+var player1 = true;
 
+// Add click event listeners to tic tac toe table
+var cells = document.getElementsByTagName('td');
+
+for (let cell of cells) {
+  cell.addEventListener('click', (event) => {
+    // toggle X vs O
+    if(player1) {
+      cell.innerHTML = '__X__';
+    } else {
+      cell.innerHTML = '__O__';
+    }
+    player1 = !player1;
+  })
+};
