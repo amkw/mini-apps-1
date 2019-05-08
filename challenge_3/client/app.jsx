36870c0ac85cae675c2eabe679e1be2de2205214
Alyssa Wei <wei.alyssa@gmail.com>
2019-05-08 16:47:40 -0700

    Built out F1 react component

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,66 @@
-var reactDOM = require('../public/react-dom.development');
+class F1 extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    };
+
+    this.handleName = this.handleName.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleName(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleEmail(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handlePassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('Account created. Name: ' + this.state.name + ' Email: ' + this.state.email + ' Password: ' + this.state.password);
+    event.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Create an account!</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type='text' value={this.state.name} onChange={this.handleName} />
+          </label> <br/>
+          <label>
+            Email:
+            <input type='email' value={this.state.email} onChange={this.handleEmail} />
+          </label> <br/>
+          <label>
+            Password:
+            <input type='text' value={this.state.text} onChange={this.handlePassword} />
+          </label> <br/>
+          <input type='submit' value='next'/>
+        </form>
+      </div>
+    )}
+};
+
+var App = () => (
+  <div>
+    {/* <button id='checkout'>Checkout Now!</button> */}
+    < F1 />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 var port = 3000;
 
+var indexhtmlpath = path.join(__dirname,'/public/index.html');
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/home', (req, res) => {
+  console.log('GET to /home');
+  res.sendFile(indexhtmlpath);
 });
 
 app.listen(port);
