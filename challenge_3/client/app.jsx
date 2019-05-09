7716b94f633035076da0f33d0223bcc6105ef3bf
Alyssa Wei <wei.alyssa@gmail.com>
2019-05-08 17:12:29 -0700

    Can render F2 and F3, does not yet flow to each other

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,3 +1,12 @@
+var App = () => (
+  <div>
+    <button id='checkout'>Checkout Now!</button>
+    < F1 />
+    < F2 />
+    < F3 />
+  </div>
+);
+
 class F1 extends React.Component {
   constructor(props) {
     super(props);
@@ -41,26 +50,177 @@ class F1 extends React.Component {
           <label>
             Name:
             <input type='text' value={this.state.name} onChange={this.handleName} />
-          </label> <br/>
+          </label> <br />
           <label>
             Email:
             <input type='email' value={this.state.email} onChange={this.handleEmail} />
-          </label> <br/>
+          </label> <br />
           <label>
             Password:
-            <input type='text' value={this.state.text} onChange={this.handlePassword} />
+            <input type='password' value={this.state.password} onChange={this.handlePassword} />
+          </label> <br />
+          <input type='submit' value='next' />
+        </form>
+      </div>
+    )
+  }
+};
+
+class F2 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      address_line1: '',
+      address_line2: '',
+      city: '',
+      state: '',
+      zip: '',
+      phone: ''
+    };
+
+    this.handleAddLine1 = this.handleAddLine1.bind(this);
+    this.handleAddLine2 = this.handleAddLine2.bind(this);
+    this.handleCity = this.handleCity.bind(this);
+    this.handleState = this.handleState.bind(this);
+    this.handleZip = this.handleZip.bind(this);
+    this.handlePhone = this.handlePhone.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleAddLine1(event) {
+    this.setState({ address_line1: event.target.value });
+  }
+
+  handleAddLine2(event) {
+    this.setState({ address_line2: event.target.value });
+  }
+
+  handleCity(event) {
+    this.setState({ city: event.target.value });
+  }
+
+  handleState(event) {
+    this.setState({ state: event.target.value });
+  }
+
+  handleZip(event) {
+    this.setState({ zip: event.target.value });
+  }
+
+  handlePhone(event) {
+    this.setState({ phone: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(JSON.stringify(this.state));
+    event.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Shipping Address</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Address Line 1:
+            <input type='text' value={this.state.address_line1} onChange={this.handleAddLine1} />
+          </label> <br />
+          <label>
+            Address Line 2:
+            <input type='text' value={this.state.address_line2} onChange={this.handleAddLine2} />
+          </label> <br />
+          <label>
+            City:
+            <input type='text' value={this.state.city} onChange={this.handleCity} />
+          </label> <br />
+          <label>
+            State:
+            <input type='text' value={this.state.state} onChange={this.handleState} />
+          </label> <br />
+          <label>
+            Zip Code:
+            <input type='number' value={this.state.zip} onChange={this.handleZip} />
+          </label> <br />
+          <label>
+            Phone Number:
+            <input type='text' value={this.state.phone} onChange={this.handlePhone} />
+          </label> <br />
+          <input type='submit' value='next' />
+        </form>
+      </div>
+    )
+  }
+};
+
+class F3 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cc: '',
+      exp: '',
+      cvv: '',
+      zip: ''
+    };
+
+    this.handleCC = this.handleCC.bind(this);
+    this.handleExp = this.handleExp.bind(this);
+    this.handleCVV = this.handleCVV.bind(this);
+    this.handleZip = this.handleZip.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleCC(event) {
+    this.setState({ cc: event.target.value });
+  }
+
+  handleExp(event) {
+    this.setState({ exp: event.target.value });
+  }
+
+  handleCVV(event) {
+    this.setState({ cvv: event.target.value });
+  }
+
+  handleZip(event) {
+    this.setState({ zip: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('Account created. CC: ' + this.state.cc + ' Exp: ' + this.state.exp + ' CVV: ' + this.state.cvv + ' Zip: ' + this.state.zip);
+    event.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Billing Info</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Credit Card Number:
+            <input type='number' value={this.state.cc} onChange={this.handleCC} />
           </label> <br/>
+          <label>
+            Expiration Date:
+            <input type='text' value={this.state.exp} onChange={this.handleExp} />
+          </label> <br/>
+          <label>
+            CVV:
+            <input type='number' value={this.state.cvv} onChange={this.handleCVV} />
+          </label> <br/>
+          <label>
+            Zip Code:
+            <input type='number' value={this.state.zip} onChange={this.handleZip} />
+          </label> <br />
           <input type='submit' value='next'/>
         </form>
       </div>
     )}
 };
-
-var App = () => (
-  <div>
-    {/* <button id='checkout'>Checkout Now!</button> */}
-    < F1 />
-  </div>
-);
 
 ReactDOM.render(<App />, document.getElementById('app'));
